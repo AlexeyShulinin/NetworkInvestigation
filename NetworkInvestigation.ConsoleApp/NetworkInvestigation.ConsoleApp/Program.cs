56b65fce334990898da2e796b20cc1fe335123f5
AlexeyShulinin <alexey.shulinin@solbeg.com>
2022-10-31 23:16:00 +0300

    Initial commit

---
+++ NetworkInvestigation.ConsoleApp/NetworkInvestigation.ConsoleApp/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace NetworkInvestigation.ConsoleApp
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("test");
+        }
+    }
+}
